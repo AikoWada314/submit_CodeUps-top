79b438f48a81e7bff074163600c23826ab3a321e
AikoWada314 <aiko.lifestyle314@gmail.com>
2024-04-01 14:02:56 +0900

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -29,13 +29,8 @@ $(window).scroll(function () {
 
 
 //ドロワーメニュー
-$("#MenuButton").click(function () {
-  // $(".drawer-menu").toggleClass("is-show");
-  // $(".drawer-menu").toggleClass("is-show");
-  $(".js-drawer-open").toggleClass("open");
-  $(".drawer-menu").toggleClass("open");
-  $("html").toggleClass("is-fixed");
-
+$(".js-hamburger").click(function () {
+  $(".js-sp-nav").toggleClass("is-active");
 });
 
 $(function () {
@@ -43,7 +38,6 @@ $(function () {
   $(".js-hamburger").click(function () {
     $(this).toggleClass("is-active");
     $("html").toggleClass("is-fixed");
-    $(".js-sp-nav").toggleClass("is-active");
   });
 });
 
